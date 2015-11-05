e2a4da784a4d5a1b6a6f39efa0fb17d77f80af5a
fbukevin <fbukevin@gmail.com>
2015-11-05 09:36:04 +0800

    改用 d3.js 的 Sankey Diagram 讓流量加上標記

---
+++ d3_version/data.js
@@ -0,0 +1,23 @@
+
+var data = [
+          { "source":'台北', "target": '出口', "value": 5.9990 }, { "source": '台北', "target": '進口', "value": 9.8972 },
+          { "source": '三重', "target": '出口', "value": 4.0520 }, { "source": '三重', "target": '進口', "value": 7.5096 },
+          { "source": '五股及五股轉接道', "target": '出口', "value": 12.7329 }, { "source": '五股及五股轉接道', "target": '進口', "value": 3.1801 },
+          { "source": '泰山轉接道', "target": '進口', "value": 2.9948 },
+          { "source": '林口', "target": '出口', "value": 13.4038}, { "source": '林口', "target": '進口', "value": 12.6267},
+          { "source": '桃園', "target": '出口', "value": 9.7262 }, { "source": '桃園', "target": '進口', "value": 8.6690 }, 
+          { "source": '桃園機場', "target": '出口', "value": 6.5995 }, { "source": '桃園機場', "target": '進口', "value": 8.4521 },
+          { "source": '內壢', "target": '出口', "value": 6.5811}, { "source": '內壢', "target": '進口', "value": 7.6167},
+          { "source": '中壢', "target": '出口', "value": 5.0653 }, { "source": '中壢', "target": '進口', "value": 5.7724},
+          { "source": '平鎮系統', "target": '出口', "value": 4.2092 }, { "source": '平鎮系統', "target": '進口', "value": 4.8456 },
+          { "source": '幼獅', "target": '出口', "value": 1.3495 }, { "source": '幼獅', "target": '進口', "value": 3.3576},
+          { "source": '楊梅', "target": '出口', "value": 7.154 }, { "source": '楊梅', "target": '進口', "value": 3.6845 },
+          { "source": '高架楊梅端', "target": '出口', "value": 4.2459 }, { "source": '高架楊梅端', "target": '進口', "value": 0 },
+          { "source": '湖口', "target": '出口', "value": 4.5985 }, { "source": '湖口', "target": '進口', "value": 1.0981 },
+          { "source": '竹北', "target": '出口', "value": 5.1938 }, { "source": '竹北', "target": '進口', "value": 4.4382 },
+          { "source": '新竹', "target": '出口',  "value": 6.2756}, { "source": '新竹', "target": '進口', "value": 1.00445 },
+          { "source": '新竹系統', "target": '出口', "value": 4.4123 }, { "source": '新竹系統', "target": '進口', "value": 8.3806 }
+    ];
+ 
+
+// 太多資料會有 <rec> 屬性給值是負值的問題，請到 customized 調高 height 
