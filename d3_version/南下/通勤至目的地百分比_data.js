4acde2e20e9706288e5a7425a07b0018a5b0474d
fbukevin <fbukevin@gmail.com>
2015-11-15 19:32:49 +0800

    新增南北向通勤至目的地百分比

---
+++ d3_version/南下/通勤至目的地百分比_data.js
@@ -0,0 +1,34 @@
+
+var data = [
+          { "source":'大台北', "target": '三重', "value": 8.04},
+          { "source":'大台北', "target": '五股及五股轉接道', "value": 7.99},
+          { "source":'大台北', "target": '泰山轉接道', "value": 7.56},
+          { "source":'大台北', "target": '林口', "value": 11.59},
+          { "source":'三重', "target": '出口總量', "value": 8.04},
+          { "source":'五股及五股轉接道', "target": '出口總量', "value": 7.99} ,
+          { "source":'泰山轉接道', "target": '出口總量', "value": 7.56},
+          { "source":'林口', "target": '出口總量', "value": 11.59},
+          { "source":'大桃園', "target": '桃園', "value": 10.77},
+          { "source":'大桃園', "target": '機場系統', "value": 4.09},
+          { "source":'大桃園', "target": '內壢', "value": 8.95},
+          { "source":'大桃園', "target": '中壢', "value": 7.68},
+          { "source":'大桃園', "target": '平鎮系統', "value": 6.54},
+          { "source":'大桃園', "target": '幼獅', "value": 1.5},
+          { "source":'大桃園', "target": '楊梅', "value": 9.27},
+          { "source":'桃園', "target": '出口總量', "value": 10.77},
+          { "source":'機場系統', "target": '出口總量', "value": 4.09},
+          { "source":'內壢', "target": '出口總量', "value": 8.95},
+          { "source":'中壢', "target": '出口總量', "value": 7.68},
+          { "source":'平鎮系統', "target": '出口總量', "value": 6.54},
+          { "source":'幼獅', "target": '出口總量', "value": 1.5},
+          { "source":'楊梅', "target": '出口總量', "value": 9.27},
+          { "source":'大新竹', "target": '湖口', "value": 2.32},
+          { "source":'大新竹', "target": '竹北', "value": 5.02},
+          { "source":'大新竹', "target": '新竹', "value": 8.3},
+          { "source":'湖口', "target": '出口總量', "value": 2.32},
+          { "source":'竹北', "target": '出口總量', "value": 5.02},
+          { "source":'新竹', "target": '出口總量', "value": 8.3}
+    ];
+
+
+// 太多資料會有 <rec> 屬性給值是負值的問題，請到 customized 調高 height
